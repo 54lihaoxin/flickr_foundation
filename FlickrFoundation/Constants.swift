23e7b031d18ce3364848d440c636003eaf2aa58c
Haoxin Li <54lihaoxin@gmail.com>
2019-02-26 19:20:06 -0800

    add some basic definitions

---
+++ FlickrFoundation/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FlickrFoundation
+//
+//  Created by Haoxin Li on 2/26/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+import Foundation
+
+enum APIKeys {
+    static let flickrAPIKey = "1508443e49213ff84d566777dc211f2a"
+}
---
+++ FlickrFoundation/KeywordSearchManager.swift
@@ -0,0 +1,13 @@
+//
+//  KeywordSearchManager.swift
+//  FlickrFoundation
+//
+//  Created by Haoxin Li on 2/26/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+import Foundation
+
+public final class KeywordSearchManager {
+    public static let shared = KeywordSearchManager()
+}

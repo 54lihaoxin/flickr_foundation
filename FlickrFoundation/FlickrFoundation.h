ae98c7f41eb9c968e75492ad2a3433ce24b1f8be
Haoxin Li <54lihaoxin@gmail.com>
2019-02-26 18:23:09 -0800

    create the empty framework

---
+++ FlickrFoundation/FlickrFoundation.h
@@ -0,0 +1,19 @@
+//
+//  FlickrFoundation.h
+//  FlickrFoundation
+//
+//  Created by Haoxin Li on 2/26/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlickrFoundation.
+FOUNDATION_EXPORT double FlickrFoundationVersionNumber;
+
+//! Project version string for FlickrFoundation.
+FOUNDATION_EXPORT const unsigned char FlickrFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlickrFoundation/PublicHeader.h>
+
+
